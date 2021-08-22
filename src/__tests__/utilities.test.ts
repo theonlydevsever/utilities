d1f4e95b450f1a5ea77b26fcd8c79694a47f1ffb
Dylan O'Leary <dylanolearydev@gmail.com>
2021-08-22 12:54:10 -0400

    Add tests for 'forceArray' function

--- src/__tests__/utilities.test.ts
+++ src/__tests__/utilities.test.ts
@@ -1,7 +1,20 @@
 import { forceArray } from "../utilities";
 
 describe("Utilities Testing", () => {
-    test("Ping", () => {
-        expect("").toHaveLength(0).toEqual("");
-    });
+    describe("forceArray", () => {
+        test("returns an array if an array of values is passed", () => {
+            const expected = [1, "Two", true];
+            const actual = forceArray(expected);
+
+            expect(actual).toHaveLength(expected.length).toEqual(expected);
+        });
+
+        test("returns an array if a single value is passed", () => {
+            const value = "Colors 2";
+            const expected = [value];
+            const actual = forceArray(value);
+
+            expect(actual).toHaveLength(expected.length).toEqual(expected);
+        });
+    }); // close describe("forceArray")
 }); // close describe("Utilities Testing")
